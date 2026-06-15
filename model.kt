59968645804b6a0983f4d2f29e93fa3a6e8732cd
amiralimahdavi368-gif <amiralimahdavi368@gmail.com>
2026-06-15 12:30:42 +0330

    Update model.kt

--- model.kt
+++ model.kt
@@ -1,1 +1,14 @@
+data class Movie(
+    val title: String,
+    val poster: String,
+    val url: String
+)
 
+data class Category(
+    val name: String,
+    val movies: List<Movie>
+)
+
+data class Root(
+    val categories: List<Category>
+)
